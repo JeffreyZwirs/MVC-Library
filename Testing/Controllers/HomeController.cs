06eca8da1ca90589ef17fa49c786f343ebabfb9b
JeffreyZwirs <91082602@mydavinci.nl>
2015-11-17 22:28:55 +0100

    Project Initialization

---
+++ Testing/Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Testing.Models;
+
+namespace Testing.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DatabaseContext db = new DatabaseContext();
+
+        // GET: Books
+        public ActionResult Index()
+        {
+            return View(db.books.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(DatabaseContext.Books book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.books.Add(book);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public ActionResult Edit(int id = 0)
+        {
+            Session["BookId"] = id;
+            DatabaseContext.Books book = db.books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            
+            return View(book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(DatabaseContext.Books book)
+        {
+            book.BookId = (int)Session["BookId"];
+            if (ModelState.IsValid)
+            {
+                db.Entry(book).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public ActionResult Delete(int id = 0)
+        {
+            DatabaseContext.Books book = db.books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            
+            return View(book);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id = 0)
+        {
+            DatabaseContext.Books book = db.books.Find(id);
+
+            db.books.Remove(book);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public string details()
+        {
+            //List<string> list = new List<string>();
+            return "Work in process";
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Testing/Models/DatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Testing.Models
+{
+    public class DatabaseContext :DbContext
+    {
+        public DatabaseContext()
+            : base("DefaultConnection")
+        {
+        }
+        public DbSet<Books> books { get; set; }
+
+        [Table("Books")]
+        public class Books
+        {
+            [Key]
+            public int BookId { get; set; }
+            public string BookName { get; set; }
+            public string BookGenre { get; set; }
+        }
+    }
+}
